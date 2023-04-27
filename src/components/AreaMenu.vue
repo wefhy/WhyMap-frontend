357149ea2b08ad08c19295ba3fa2ed84101fcc13
wefhy <>
2023-04-27 23:39:50 +0200

    fix link

--- src/components/AreaMenu.vue
+++ src/components/AreaMenu.vue
@@ -1,15 +1,30 @@
 <!-- Copyright (c) 2023 wefhy -->
 
 <template>
-  <div>Area: {{ areaStart }} - {{ areaEnd }}</div>
-  <div>Szie: {{ size }} ({{width}}x{{height}})</div>
+    <div>Area: {{ areaStart }} - {{ areaEnd }}</div>
+    <div>Size: {{ size }} ({{width}}x{{height}})</div>
     <select v-model="format">
         <option value="jpeg">JPEG</option>
         <option value="png">PNG</option>
+        <option value="webp" disabled="disabled">WEBP</option>
+        <option value="tiff" disabled="disabled">TIFF</option>
+        <option value="gif" disabled="disabled">GIF</option>
+        <option value="bmp" disabled="disabled">BMP</option>
     </select>
-  <button @click="exportArea">Export area</button>
-<!--  <button @click="showStats">Show statistics</button>-->
-
+    <select v-model="scale">
+        <option value="0.0625" disabled="disabled">1/16x</option>
+        <option value="0.125" disabled="disabled">1/8x</option>
+        <option value="0.25" disabled="disabled">1/4x</option>
+        <option value="0.5" disabled="disabled">1/2x</option>
+        <option value="1">1x</option>
+        <option value="2" disabled="disabled">2x</option>
+        <option value="4" disabled="disabled">4x</option>
+        <option value="8" disabled="disabled">8x</option>
+        <option value="16">16x</option>
+    </select>
+    <div>Resolution: {{ outX }}x{{ outY }} ({{ megapixel }}MP)</div>
+    <button @click="exportArea">Export area</button>
+    <button @click="showStats">Show statistics</button>
 </template>
 
 <script>
@@ -22,6 +37,7 @@ export default {
             posStart: "AA",
             posEnd: "BB",
             format: "jpeg",
+            scale: 1,
             width: "calculating...",
             height: "calculating...",
             areaStart: "calculating...",
@@ -29,16 +45,27 @@ export default {
             size: "calculating..."
         }
     },
+    computed: {
+        outX() {
+            return Math.ceil(this.width * parseFloat(this.scale))
+        },
+        outY() {
+            return Math.ceil(this.height * parseFloat(this.scale))
+        },
+        megapixel() {
+            return (this.outX * this.outY / 1000000).toFixed(1)
+        }
+    },
     methods: {
         showStats() {
-            fetch(`${host}/getAreaStats/${this.areaStart[0]}/${this.areaStart[1]}/${this.areaEnd[0]}/${this.areaEnd[1]}`)
+            fetch(`${host}/analyzeArea/${this.areaStart[0]}/${this.areaStart[1]}/${this.areaEnd[0]}/${this.areaEnd[1]}`)
                 .then(response => response.json())
                 .then(data => {
-                    alert(JSON.stringify(data))
+                    alert(JSON.stringify(data, null, 2))
                 })
         },
         exportArea() {
-            open(`${host}/exportArea/${this.areaStart[0]}/${this.areaStart[1]}/${this.areaEnd[0]}/${this.areaEnd[1]}/${this.format}`, '_blank').focus()
+            open(`${host}/exportArea/${this.areaStart[0]}/${this.areaStart[1]}/${this.areaEnd[0]}/${this.areaEnd[1]}/${this.format}/${this.scale}`, '_blank').focus()
             // fetch(`${host}/exportArea/${this.areaStart[0]}/${this.areaStart[1]}/${this.areaEnd[0]}/${this.areaEnd[1]}/${this.format}`)
         }
     },
